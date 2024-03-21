34f9abbc1331494c89ad6a3e80bdac8814b9cd58
Yann Jouanique <yann.jouanique@gmail.com>
2024-03-21 09:11:53 +0200

    Switch to OO

--- uno.py
+++ uno.py
@@ -2,25 +2,26 @@ from rich import print
 from random import shuffle, randint
 
 COLORS = ["red", "green", "blue", "yellow"]
+WILDCARD_COLOR = "gray"
 CARD_TYPES = [
     # Number cards
-    {"name": "0", "colored": True, "quantity": 1, "value": 0},
-    {"name": "1", "colored": True, "quantity": 2, "value": 1},
-    {"name": "2", "colored": True, "quantity": 2, "value": 2},
-    {"name": "3", "colored": True, "quantity": 2, "value": 3},
-    {"name": "4", "colored": True, "quantity": 2, "value": 4},
-    {"name": "5", "colored": True, "quantity": 2, "value": 5},
-    {"name": "6", "colored": True, "quantity": 2, "value": 6},
-    {"name": "7", "colored": True, "quantity": 2, "value": 7},
-    {"name": "8", "colored": True, "quantity": 2, "value": 8},
-    {"name": "9", "colored": True, "quantity": 2, "value": 9},
+    {"name": "0", "wildcard": False, "quantity": 1, "value": 0},
+    {"name": "1", "wildcard": False, "quantity": 2, "value": 1},
+    {"name": "2", "wildcard": False, "quantity": 2, "value": 2},
+    {"name": "3", "wildcard": False, "quantity": 2, "value": 3},
+    {"name": "4", "wildcard": False, "quantity": 2, "value": 4},
+    {"name": "5", "wildcard": False, "quantity": 2, "value": 5},
+    {"name": "6", "wildcard": False, "quantity": 2, "value": 6},
+    {"name": "7", "wildcard": False, "quantity": 2, "value": 7},
+    {"name": "8", "wildcard": False, "quantity": 2, "value": 8},
+    {"name": "9", "wildcard": False, "quantity": 2, "value": 9},
     # Colored specials
-    {"name": "+2", "colored": True, "quantity": 2, "value": 20},
-    {"name": "🔄", "colored": True, "quantity": 2, "value": 20},
-    {"name": "🚫", "colored": True, "quantity": 2, "value": 20},
+    {"name": "+2", "wildcard": False, "quantity": 2, "value": 20},
+    {"name": "🔄", "wildcard": False, "quantity": 2, "value": 20},
+    {"name": "🚫", "wildcard": False, "quantity": 2, "value": 20},
     # Black cards
-    {"name": "+4", "colored": False, "quantity": 4, "value": 40},
-    {"name": "🌈", "colored": False, "quantity": 4, "value": 40},
+    {"name": "+4", "wildcard": True, "quantity": 4, "value": 40},
+    {"name": "🌈", "wildcard": True, "quantity": 4, "value": 40},
 ]
 PLAYERS = [
     "You",  # The only human player
@@ -38,136 +39,183 @@ PLAYERS = [
 INITIAL_CARDS = 7
 
 
-def create_deck():
-    deck = []
-    for card in CARD_TYPES:
-        if card["colored"]:
-            for color in COLORS:
+class UnoCard:
+    def __init__(self, name, color, value):
+        self.name = name
+        self.color = color
+        self.value = value
+
+    def can_play(self, top_card):
+        return (
+            self.name == top_card.name
+            or self.color == top_card.color
+            or self.color == WILDCARD_COLOR
+        )
+
+    def print_card(self):
+        print(f"[white on {self.color}]{self.name:^3}")
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.color}"
+
+
+class UnoPlayer:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def print_hand(self, top_card=None):
+        for i, card in enumerate(self.hand):
+            if card.can_play(top_card):
+                print(f"[green bold]{i+1:>2}. ", end="")
+            else:
+                print(f" x. ", end="")
+            card.print_card()
+
+    def can_play(self, top_card):
+        return any(card.can_play(top_card) for card in self.hand)
+
+    def play_card(self, card_index, game, color_override=None):
+        card = self.hand[card_index]
+        if card.can_play(game.top_card):
+            self.hand.pop(card_index)
+
+            # Override color if needed
+            if color_override:
+                card.color = color_override
+
+            game.play_card(card)
+
+            return card
+
+    def draw_card(self, card):
+        self.hand.append(card)
+
+    def has_won(self):
+        return len(self.hand) == 0
+
+    def auto_play(self, game):
+        playable_cards = [card for card in self.hand if card.can_play(game.top_card)]
+        if playable_cards:
+            # TODO: be smarter, e.g. play the highest value but save wildcards for last...
+            # Play the first eligible one...
+            card = playable_cards[0]
+
+            # If the card is a wildcard, choose a random color to play
+            color = None
+            if card.color == WILDCARD_COLOR:
+                color = COLORS[randint(0, 3)]
+
+            return self.play_card(self.hand.index(card), game, color)
+        return None
+
+    def score(self):
+        return sum(card.value for card in self.hand)
+
+
+class UnoGame:
+    def __init__(self):
+        self.deck = self.create_deck()
+        self.players = [UnoPlayer(player) for player in PLAYERS]
+        self.top_card = None
+        self.current_player_idx = 0
+        self.direction = 1
+        self.winner = None
+        self.penalty = None
+
+    def create_deck(self):
+        deck = []
+        for card in CARD_TYPES:
+            if card["wildcard"]:
                 for _ in range(card["quantity"]):
-                    deck.append(
-                        {"name": card["name"], "color": color, "value": card["value"]}
-                    )
-        else:
-            for _ in range(card["quantity"]):
-                deck.append({"name": card["name"], "color": "grey"})
+                    deck.append(UnoCard(card["name"], WILDCARD_COLOR, card["value"]))
+            else:
+                for color in COLORS:
+                    for _ in range(card["quantity"]):
+                        deck.append(UnoCard(card["name"], color, card["value"]))
 
-    shuffle(deck)
-    return deck
+        shuffle(deck)
+        return deck
 
+    def print_top_card(self):
+        self.top_card.print_card()
 
-def print_card(card):
-    if card and "name" in card and "color" in card:
-        print(f"[white on {card['color']}]{card['name']:^3}", end="")
-
-
-def print_top_card(game):
-    top_card = game["top_card"]
-    print_card(top_card)
-    print()
-
-
-def print_hand(game, player):
-    for i, card in enumerate(game["players"][player]["hand"]):
-        if can_play_card(game, card):
-            print(f"[green bold]{i+1:>2}. ", end="")
-        else:
-            print(f"[gray] x. ", end="")
-        print_card(card)
-        print()
-
-
-def can_play_card(game, card):
-    top_card = game["top_card"]
-    return (
-        card["name"] == top_card["name"]
-        or card["color"] == top_card["color"]
-        or card["color"] == "grey"
-    )
-
-
-def player_can_play(game, player):
-    player_hand = game["players"][player]["hand"]
-    return any(can_play_card(game, card) for card in player_hand)
-
-
-def deal_card(game, player):
-    if game["deck"] and len(game["deck"]):
-        game["players"][player]["hand"].append(game["deck"].pop())
-        return True
-    return False
-
-
-def draw_card(game):
-    if game["deck"] and len(game["deck"]):
-        game["top_card"] = game["deck"].pop()
-        return True
-    return False
-
-
-def play_card(game, player, card_index, color_override=None):
-    if can_play_card(game, game["players"][player]["hand"][card_index]):
-        card = game["players"][player]["hand"].pop(card_index)
-
-        # Override color if needed
-        if color_override:
-            card["color"] = color_override
-
-        game["top_card"] = card
+    def play_card(self, card):
+        # NOTE: we're not checking card legality here, because its color might have been overridden...
+        self.top_card = card
 
         # Change game direction if needed
-        if card["name"] == "🔄":
-            game["direction"] *= -1
+        if card.name == "🔄":
+            self.direction *= -1
 
         # Set the penalty for next player if any
-        if card["name"] in ["+2", "+4", "🚫"]:
-            game["penalty"] = card["name"]
+        match card.name:
+            case "+4":
+                self.penalty = 4
+            case "+2":
+                self.penalty = 2
+            case "🚫":
+                self.penalty = 0
         # The reverse acts as a skip if there's only 2 players
-        if card["name"] == "🔄" and len(game["players"]) == 2:
-            game["penalty"] = "🚫"
+        if card.name == "🔄" and len(self.players) == 2:
+            self.penalty = 0
 
-        print(f"{game['players'][player]['name']} played ", end="")
-        print_card(card)
-        print()
+        return card
 
-        return True
-    return False
+    def can_play_card(self, card):
+        return card.can_play(self.top_card)
 
-
-def auto_play(game, player):
-    player_hand = game["players"][player]["hand"]
-    playable_cards = [card for card in player_hand if can_play_card(game, card)]
-    if playable_cards:
-        # TODO: be smarter, e.g. play the highest value but save wildcards for last...
-        # Play the first eligible one...
-        card = playable_cards[0]
-
-        # If the card is a wildcard, choose a random color to play
-        color = None
-        if card["color"] == "grey":
-            color = COLORS[randint(0, 3)]
-
-        return play_card(game, player, player_hand.index(card), color)
-    return False
-
-
-def check_winner(game):
-    for player in game["players"]:
-        if len(player["hand"]) == 0:
-            game["winner"] = player
+    def deal_card_to_player(self, player_idx):
+        card = self.draw_card()
+        if card:
+            self.players[player_idx].draw_card(card)
             return True
-    return False
+        return False
 
+    def deal_card(self):
+        return self.deal_card_to_player(self.current_player_idx)
 
-def next_player(game):
-    game["current_player"] = (game["current_player"] + game["direction"]) % len(
-        game["players"]
-    )
+    def has_winner(self):
+        for player in self.players:
+            if player.has_won():
+                self.winner = player
+                return True
+        return False
+
+    def next_player(self):
+        self.current_player_idx = (self.current_player_idx + self.direction) % len(
+            self.players
+        )
+
+    def play_new_card(self):
+        card = self.draw_card()
+        if card:
+            self.top_card = card
+            return True
+        return False
+
+    def draw_card(self):
+        if self.deck and len(self.deck):
+            return self.deck.pop()
+        return None
+
+    def current_player(self):
+        return self.players[self.current_player_idx]
+
+    def apply_penalty(self):
+        if self.penalty is not None:
+            for _ in range(self.penalty):
+                self.deal_card()
+            self.penalty = None
+            self.next_player()
+            return True
+        return False
 
 
 def prompt_color():
     print("[bold green]Choose a color to play:")
     for i, color in enumerate(COLORS):
-        print(f"[white on {color}]{i+1:^2}")
+        print(f"[white on {color}]{i+1:^3}")
     while True:
         try:
             return COLORS[int(input()) - 1]
@@ -175,106 +223,94 @@ def prompt_color():
             print("[red]Invalid input, try again")
 
 
-current_game = {
-    "deck": create_deck(),
-    "players": [{"name": player, "hand": []} for player in PLAYERS],
-    "top_card": None,
-    "current_player": 0,
-    "direction": 1,
-    "winner": None,
-    "penalty": None,
-}
+def prompt_card(player, game):
+    while True:
+        try:
+            card_index = int(input()) - 1
+            card = player.hand[card_index]
+
+            # If the card is a wildcard, prompt for color to play
+            if card.can_play(game.top_card):
+                color = None
+                if card.color == WILDCARD_COLOR:
+                    color = prompt_color()
+
+                return card_index, color
+            else:
+                print("[red]You can't play that card!")
+        except Exception:
+            print("[red]Invalid input, try again")
+
+
+game = UnoGame()
 
 # Deal cards
 for _ in range(INITIAL_CARDS):
-    for player in range(len(PLAYERS)):
-        # print(f"Dealing to {PLAYERS[player]}")
-        deal_card(current_game, player)
+    for player in range(len(game.players)):
+        game.deal_card_to_player(player)
 
 # Place top card
-draw_card(current_game)
+game.play_new_card()
 
-while not current_game["winner"]:
-    player = current_game["current_player"]
-    print(f"[bold blue]{current_game['players'][player]['name']}'s turn")
+while not game.has_winner():
+    player = game.current_player()
+    print(f"[bold blue]{player.name}'s turn")
 
     # print(current_game)
 
     print("Top card: ", end="")
-    print_top_card(current_game)
+    game.print_top_card()
 
     # Apply penalties if needed
-    if current_game["penalty"]:
-        match current_game["penalty"]:
-            case "+4":
+    if game.penalty is not None:
+        match game.penalty:
+            case 4:
                 print("[red]Penalty: 4 cards 🥵")
-                deal_card(current_game, player)
-                deal_card(current_game, player)
-                deal_card(current_game, player)
-                deal_card(current_game, player)
-            case "+2":
+            case 2:
                 print("[red]Penalty: 2 cards 😓")
-                deal_card(current_game, player)
-                deal_card(current_game, player)
-            case "🚫":
+            case 0:
                 print("[red]Penalty: Skipping 😔")
-        current_game["penalty"] = None
-        next_player(current_game)
+        game.apply_penalty()
         continue
 
     # Draw if you cannot play anything
-    if not player_can_play(current_game, player):
+    if not player.can_play(game.top_card):
         print("[yellow]Can't play, drawing")
-        deal_card(current_game, player)
+        game.deal_card()
 
     # Skip if you still cannot play anything
-    if not player_can_play(current_game, player):
+    if not player.can_play(game.top_card):
         print("[yellow]Still can't play, skipping")
-        next_player(current_game)
+        game.next_player()
         continue
 
-    if player == 0:
+    played_card = None
+    if game.current_player_idx == 0:
         # It's the only human player, show hand and prompt for play
         print("Your hand:")
-        print_hand(current_game, player)
+        player.print_hand(game.top_card)
 
         print("[green bold]Which card do you want to play?")
-        valid_play = False
-        while not valid_play:
-            try:
-                card_index = int(input()) - 1
-
-                # If the card is a wildcard, prompt for color to play
-                color = None
-                if (
-                    current_game["players"][player]["hand"][card_index]["color"]
-                    == "grey"
-                ):
-                    color = prompt_color()
-
-                if play_card(current_game, player, card_index, color):
-                    valid_play = True
-
-                else:
-                    print("[red]You can't play that card!")
-            except Exception:
-                print("[red]Invalid input, try again")
+        idx, color_override = prompt_card(player, game)
+        played_card = player.play_card(idx, game, color_override)
     else:
         # It's an AI player, play automatically
-        auto_play(current_game, player)
+        played_card = player.auto_play(game)
+
+    if played_card:
+        print(f"{player.name} played: ", end="")
+        played_card.print_card()
 
     # Check if anyone won
-    if check_winner(current_game):
-        print(f"[bold green]{current_game['winner']['name']} wins! 🎉")
+    if game.has_winner():
+        print(f"[bold green]{game.winner.name} wins! 🎉")
         break
 
-    next_player(current_game)
+    game.next_player()
 
 
 # Show scores
 print()
 print("[blue]Final scores:")
-for player in current_game["players"]:
-    print(
-        f"{player['name']}: {sum(card['value'] for card in player['hand'])} {'🏆' if not len(player['hand']) else ''}"
-    )
+for player in game.players:
+    print(f"{player.name}: {player.score()} {'🏆' if player.has_won() else ''}")
